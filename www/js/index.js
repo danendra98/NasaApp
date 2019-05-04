8f3460cb0f21e12f2eae68403927c5f979e94809
Davis Danendra <48470926+danendra98@users.noreply.github.com>
2019-05-04 21:23:36 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=RsISwWwII6mriu1rLFGRJtlVIWzArIqgkVUcoVZ8";
         
         $("#loadImage").click(function(){
             $.ajax({
